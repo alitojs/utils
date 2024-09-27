912e5db4c52252ceddfdb22fb49e7d2b138a5c9e
陈书航 <chen.shuhang@iwhalecloud.com>
2024-09-27 16:40:22 +0800

    feat: init util

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  printWidth: 130,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: 'always',
+  htmlWhitespaceSensitivity: 'ignore',
+  vueIndentScriptAndStyle: false,
+  singleAttributePerLine: false,
+  endOfLine: 'auto'
+};
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # utils
-some tools function
+
+提供部分通用工具。
+
+## 安装
+
+```bash
+yarn add @alitojs/utils
+```
+
+## 使用
+
+```js
+import { getRandom } from '@alitojs/utils';
+
+console.log(getRandom()); //aanjio
+```
